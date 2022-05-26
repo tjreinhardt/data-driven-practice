ed53759178d6c10d8ced25cceef3d485b54acb58
Edward <wedward1024@gmail.com>
2022-05-26 14:10:50 -0700

    asyncHandler

--- routes.js
+++ routes.js
@@ -3,14 +3,23 @@ const express = require('express');
 const db = require('./db/models');
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  try {
-    const books = await db.Book.findAll({ order: [['title', 'ASC']] });
-    res.render('index', { title: 'Home', books });
-  } catch (err) {
-    next(err);
-  }
-});
+// OPTION 1 try/catch method
+// router.get('/', async (req, res, next) => {
+//   try {
+//     const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+//     res.render('book-list', { title: 'Books', books });
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+// OPTION 2 asyncHandler method
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await db.Book.findAll({ order: [['title', 'ASC']] });
+  res.render('book-list', { title: 'Books', books });
+}));
 
 
 // TEST ERROR vv
